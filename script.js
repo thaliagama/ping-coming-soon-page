4b0240f8b1e32d03025bf429cff2e585358360b2
Thalia Gama <95056749+liagama@users.noreply.github.com>
2023-04-06 21:07:11 -0300

    coming-soon-page

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let btn = document.querySelector('#btn').addEventListener('click', validarEmail)
 let inputEmail = document.querySelector('.email')
 let msg = document.querySelector('.msgErro')
 
+inputEmail.addEventListener('blur', validarEmail)
+
 function validarEmail(email) {
     email = inputEmail.value
     var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -17,8 +19,6 @@ function mensagemErro(){
     msg.innerHTML = 'Please provide a valid email address'
     inputEmail.classList.add('email-erro')
 }
-
-inputEmail.addEventListener('blur', validarEmail)
 
 inputEmail.addEventListener('focus', ()=> {
     msg.innerHTML=''
